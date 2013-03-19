87c253ccf49b743e6bf568c0c4a4beb4e6f753b7
Adriano Godoy <godoy.ccp@gmail.com>
2013-03-19 15:53:43 -0300

    Projeto inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radar::Application.config.secret_token = 'cb39573196b051a1c96412656769f50a40c550c4c23046ce311ae5b04ea8f4abbbe25b3119920ef6c84d3cbd61c0a7768e89dbdd03c2ecde0411afc7b47dd10b'
